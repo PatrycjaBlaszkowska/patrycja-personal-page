354427e05e5de5f3409a5d41d4fe690da732771b
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-10-15 08:09:10 +0000

    Refactored code so it can be handled by github pages instead of heroku.

--- patrycja_personal_page/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template
-from patrycja_personal_page import app 
-
-@app.route('/')
-def index():
-    # Render index(home) page as landing page
-    return render_template('index.html')
